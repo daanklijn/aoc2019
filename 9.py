911795c03714acd0cd80826db400a7c0683e52c4
Daan Klijn <daanklijn@admins-MacBook-Pro.local>
2019-12-09 08:51:54 +0100

    Add day 9

---
+++ 9.py
@@ -0,0 +1,115 @@
+from aocd.models import Puzzle
+import numpy as np
+
+def get_value(mode,position,code,relative_base):
+    if(mode==0):
+        return code[code[position]]
+    elif(mode==1):
+        return code[position]
+    elif(mode==2):
+        return code[relative_base+code[position]]
+    else:
+        print("WRONG MODE")
+
+def get_location(mode,position,code,relative_base):
+    if(mode==0):
+        return code[position]
+    if(mode==2):
+        return code[position]+relative_base
+    else:
+        print("Cannot get location for other mode")
+
+def run_intcode(code,input_vars,position=0):
+    var_count=0
+    relative_base = 0
+
+    while(True):
+        value = code[position]
+        instruction = str(value).zfill(5)
+        opcode = int(instruction[3:5])
+        mode1 = int(instruction[2])
+        mode2 = int(instruction[1])
+        mode3 = int(instruction[0])
+
+        if(opcode == 99):
+            raise Exception()
+            
+            break
+        elif(opcode == 1):
+            a = get_value(mode1,position+1,code,relative_base)
+            b = get_value(mode2,position+2,code,relative_base)
+            c = get_location(mode3,position+3,code,relative_base)
+            code[c]=a+b
+            position+=4
+        elif(opcode == 2):
+            a = get_value(mode1,position+1,code,relative_base)
+            b = get_value(mode2,position+2,code,relative_base)
+            c = get_location(mode3,position+3,code,relative_base)
+            code[c]=a*b
+            position+=4
+        elif(opcode == 3):
+            a = get_location(mode1,position+1,code,relative_base)
+            code[a] = input_vars[var_count]
+
+            var_count+=1
+            position+=2
+        elif(opcode == 4):
+            a = get_value(mode1,position+1,code,relative_base)
+            position+=2
+            print("RETURN:" + str(a))
+            # return(a,code,position)
+        elif(opcode == 5):
+            a = get_value(mode1,position+1,code,relative_base)
+            b = get_value(mode2,position+2,code,relative_base)
+            if(a != 0):
+                position=b
+            else:
+                position+=3
+        elif(opcode == 6):
+            a = get_value(mode1,position+1,code,relative_base)
+            b = get_value(mode2,position+2,code,relative_base)
+            if(a == 0):
+                position=b
+            else:
+                position+=3
+        elif(opcode == 7):
+            a = get_value(mode1,position+1,code,relative_base)
+            b = get_value(mode2,position+2,code,relative_base)
+            c = get_location(mode3,position+3,code,relative_base)
+            if(a < b):
+                code[c]=1
+            else:
+                code[c]=0
+            position+=4
+        elif(opcode == 8):
+            a = get_value(mode1,position+1,code,relative_base)
+            b = get_value(mode2,position+2,code,relative_base)
+            c = get_location(mode3,position+3,code,relative_base)
+            if(a == b):
+                code[c]=1
+            else:
+                code[c]=0
+            position+=4
+        elif(opcode == 8):
+            a = get_value(mode1,position+1,code,relative_base)
+            b = get_value(mode2,position+2,code,relative_base)
+            c = get_location(mode3,position+3,code,relative_base)
+            if(a == b):
+                code[c]=1
+            else:
+                code[c]=0
+            position+=4
+        elif(opcode == 9):
+            a = get_value(mode1,position+1,code,relative_base)
+            relative_base+=a
+            position+=2
+        else:
+            print("wrong op" + str(instruction))
+
+data = Puzzle(year=2019, day=9).input_data.split(',')
+code = list(map(lambda x : int(x),data))+list(np.zeros(1000))
+
+# code = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99] + list(np.zeros(1000))
+# code = [1102,34915192,34915192,7,4,7,99,0]
+# code = [104,1125899906842624,99]
+run_intcode(code,[2],0)
