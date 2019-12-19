cecea0d7ea7b43826fed09378106af6c2d25c598
Daan Klijn <dklijn@adsysco.nl>
2019-12-19 09:57:06 +0100

    add day 19

---
+++ 19.py
@@ -0,0 +1,135 @@
+from aocd.models import Puzzle
+import numpy as np
+import math
+
+def get_value(mode,position,code,relative_base):
+    if(mode==0):
+        return code[code[position]]
+    elif(mode==1):
+        return code[position]
+    elif(mode==2):
+        return code[relative_base+code[position]]
+    else:
+        print("WRONG MODE")
+
+def get_location(mode,position,code,relative_base):
+    if(mode==0):
+        return code[position]
+    if(mode==2):
+        return code[position]+relative_base
+    else:
+        print("Cannot get location for other mode")
+
+def run_intcode(code,input_vars,position=0,relative_base=0):
+    var_count=0
+    prints=0
+
+    while(True):
+        value = code[position]
+        instruction = str(value).zfill(5)
+        opcode = int(instruction[3:5])
+        mode1 = int(instruction[2])
+        mode2 = int(instruction[1])
+        mode3 = int(instruction[0])
+
+        if(opcode == 99):
+            raise Exception()
+            
+            break
+        elif(opcode == 1):
+            a = get_value(mode1,position+1,code,relative_base)
+            b = get_value(mode2,position+2,code,relative_base)
+            c = get_location(mode3,position+3,code,relative_base)
+            code[c]=a+b
+            position+=4
+        elif(opcode == 2):
+            a = get_value(mode1,position+1,code,relative_base)
+            b = get_value(mode2,position+2,code,relative_base)
+            c = get_location(mode3,position+3,code,relative_base)
+            code[c]=a*b
+            position+=4
+        elif(opcode == 3):
+            a = get_location(mode1,position+1,code,relative_base)
+            code[a] = input_vars[var_count]
+
+            var_count+=1
+            position+=2
+        elif(opcode == 4):
+            a = get_value(mode1,position+1,code,relative_base)
+            position+=2
+            # print("RETURN:" + str(a))
+            return(a,code,position,relative_base)
+        elif(opcode == 5):
+            a = get_value(mode1,position+1,code,relative_base)
+            b = get_value(mode2,position+2,code,relative_base)
+            if(a != 0):
+                position=b
+            else:
+                position+=3
+        elif(opcode == 6):
+            a = get_value(mode1,position+1,code,relative_base)
+            b = get_value(mode2,position+2,code,relative_base)
+            if(a == 0):
+                position=b
+            else:
+                position+=3
+        elif(opcode == 7):
+            a = get_value(mode1,position+1,code,relative_base)
+            b = get_value(mode2,position+2,code,relative_base)
+            c = get_location(mode3,position+3,code,relative_base)
+            if(a < b):
+                code[c]=1
+            else:
+                code[c]=0
+            position+=4
+        elif(opcode == 8):
+            a = get_value(mode1,position+1,code,relative_base)
+            b = get_value(mode2,position+2,code,relative_base)
+            c = get_location(mode3,position+3,code,relative_base)
+            if(a == b):
+                code[c]=1
+            else:
+                code[c]=0
+            position+=4
+        elif(opcode == 8):
+            a = get_value(mode1,position+1,code,relative_base)
+            b = get_value(mode2,position+2,code,relative_base)
+            c = get_location(mode3,position+3,code,relative_base)
+            if(a == b):
+                code[c]=1
+            else:
+                code[c]=0
+            position+=4
+        elif(opcode == 9):
+            a = get_value(mode1,position+1,code,relative_base)
+            relative_base+=a
+            position+=2
+        else:
+            print("wrong op" + str(instruction))
+
+
+data = Puzzle(year=2019, day=19).input_data.split(',')
+code = list(map(lambda x : int(x),data))+list(np.zeros(10000))
+
+position= 0
+base = 0
+sum =0
+grid = []
+first=0
+done = False
+for x in range(500,1000):
+    grid.append([])
+    for y in range(first,1000):
+        output, code2, position2, base2 = run_intcode(code.copy(),[x,y],position,base)
+        if(output==1):
+            print(first)
+            first=x
+            output1, code2, position2, base2 = run_intcode(code.copy(),[x-99,y+99],position,base)
+            if(output1==1):
+                print(str(x-99)+","+str(y))
+                done = True
+            break
+    if(done):
+        break
+
+
